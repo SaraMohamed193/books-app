62455fe881fbe1903623daa67f04d5b0f6aab3e1
Sara Borham <saraborham193@yahoo.com>
2019-04-14 20:22:12 +0200

    Updates

---
+++ precache-manifest.27d55ecab6976b0071dbe8bf71027b1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25b476f817218dfca640",
+    "url": "/books-app/static/js/runtime~main.25b476f8.js"
+  },
+  {
+    "revision": "3800ad44417b26bf3f83",
+    "url": "/books-app/static/js/main.3800ad44.chunk.js"
+  },
+  {
+    "revision": "e6d649f488d08fe1ab4e",
+    "url": "/books-app/static/js/1.e6d649f4.chunk.js"
+  },
+  {
+    "revision": "3800ad44417b26bf3f83",
+    "url": "/books-app/static/css/main.f00691cd.chunk.css"
+  },
+  {
+    "revision": "099e73baaecd93aaf720a241b88f78b6",
+    "url": "/books-app/index.html"
+  }
+];
